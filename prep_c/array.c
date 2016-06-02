24bc8218b1d2d84923aed283c3938b18ac5bf070
satish rodd <rodd.satish@gmail.com>
2016-06-02 13:57:28 +0000

    first on 2nd june 2016

--- prep_c/array.c
+++ prep_c/array.c
@@ -5,12 +5,12 @@ void has_all_unique(char str[], int n) {
 	for (i=0; i<n;i++) {
 		for(j=i+1;j<n;j++) {
 			if (str[i]==str[j]) {
-		        printf("Not all are unique");	
+		        printf("\nNot all are unique");	
                 return;
 			}
 		}
 	}
-	printf("all are unique");	
+	printf("\nall are unique");	
 
 }
 
@@ -18,24 +18,33 @@ void has_all_unique(char str[], int n) {
 void remove_all_dup(char str[], int n) {
 	int i=0, j=0;
 	for (i=0; i<n;i++) {
-		for(j=i;j<n;j++) {
+		for(j=i+1;j<n;j++) {
 			if (str[i]==str[j]) {
 				str[j]=str[j+1];
 				j--;n--;
 			}
 		}
 	}
+        printf("\n Removed duplicates %s", str);
 }
 
-int is_anagram(char str[], int n) {
+void is_anagram(char str[],char str1[], int n,int m) {
 	int i=0, j=0, found=0;
+        if (n!=m) {
+            printf("\n Its not an anagram:%s %s", str, str1);
+            return;
+        }
 	for (i=0; i<n;i++) {
-		for(j=i;j<n;j++) {
-			if(str[i]==str[j])found=1;
+		for(j=0;j<n;j++) {
+			if(str[i]==str1[j])found=1;
 		}
-		if(!found) return(0);
+		if(!found) {
+                    printf("\nIts Not an anagram: %s %s", str, str1);
+                    return;
+                }
 	}
-	return(1);
+	printf("\nIts an anagram: %s %s", str, str1);
+        return;
 }
 
 void replace(char str[], int n) {
@@ -43,14 +52,21 @@ void replace(char str[], int n) {
 	for (i=0; i<n;i++) {
 		if(str[i]==' ')str[i]='%20';
 	}
+        printf("\nreplaced: %s",str);
 }
 
 void rotate(char matrix[10][10], int n) {
 }
 
+
 void main() {
-	char str[10] = "abcdaerfgh";
-    has_all_unique(str,9);
-        
+    char str[10] = "abcadef";
+    char str1[10] = "afecdb";
+    int n = 7, m = 6;
+    has_all_unique(str,n);
+    replace(str,n);    
+    remove_all_dup(str, n);
+    n=6;
+    is_anagram(str,str1,n,m);
     
 }
